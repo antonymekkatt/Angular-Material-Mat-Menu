85674b4265f7cab67d8313271f5ff91323808850
antonymekkatt <65225790+antonymekkatt@users.noreply.github.com>
2020-05-12 19:11:44 +0530

    Add files via upload

---
+++ src/assets/values/global-variables.ts
@@ -0,0 +1,15 @@
+export const globalVariables = Object.freeze({
+    BASE_URL: 'http://192.168.1.111:8080/ERS',
+    IMAGE_LOCATION_PATH: 'assets/images/',
+    MENU_ICON_LOCATION_PATH: 'assets/images/icons/menu_icons/',
+    ICON_LOCATION_PATH: 'assets/images/icons/',
+    LOGO: 'etbos_logo.jpg',
+    PAGE_NOT_FOUND: '404-error.jpg',
+    TABLE_LOADER_IMAGE: 'loader.gif',
+    REPORT_LOADER_IMAGE: 'loader_1.gif',
+    SHORT_DATE: 'M/d/y',
+    LONG_DATE_TIME: 'M/d/y h:mm:ss a',
+    SHORT_DATE_TIME: 'MM/dd/yyyy hh:mm',
+    MEDIUM_TIME: 'h:mm:ss a'
+    //... more of your variables
+});
